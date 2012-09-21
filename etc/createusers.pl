05f411a91f871d62f7428edbacfc2d860b2aa9ee
David Nanch <president@nanch.com>
2012-09-21 02:20:02 -0400

    Added /etc/cron.d/tarbackup entry to automatically run every 5 minutes. This creates users after they register on the website. Added a log at /var/log/tarbackup to receive output when there are errors. Updated index.html to make it cleaner. Updated the submit.php with better logic.

--- etc/createusers.pl
+++ etc/createusers.pl
@@ -1,25 +1,27 @@
 use File::Copy;
 
-$filename = "userstocreate.txt";      # the queue of users that have signed up since last run
-copy($filename, $filename . ".copy");  # make a copy to make this process as atomic as possible
+$dir = "/usr/local/etc/tarbackup/";
+$logfilename = $dir . "createdusers.log";
+$infilename = $dir . "userstocreate.txt";      # the queue of users that have signed up since last run
+copy($infilename, $infilename . ".copy");  # make a copy to make this process as atomic as possible
 
 #delete and recreate the existing file with the same permissions
-unlink($filename); 
+unlink($infilename);
 open NEWFILE, ">$infilename" or die $!;
 close NEWFILE;
-chmod 0766, $filename;
+chmod 0666, $infilename;
 
 #read the copied file and create the users that have signed up
-open INFILE, $filename . ".copy" or die $!;
-open LOGFILE, ">>createdusers.log" or die $!;
+open INFILE, $infilename . ".copy" or die $!;
+open LOGFILE, ">>$logfilename" or die $!;
 while (<INFILE>) {
- print $_;
  print LOGFILE $_;
  @parts = split(/:/);
  $user = @parts[0];
  $password = @parts[1];
- `echo $password | pw  user add -n $user -g targroup -s /bin/sh -h 0`; #create the user on the system
+ `/usr/sbin/useradd $user -g targroup`;
+ `echo $password | /usr/bin/passwd $user --stdin`;
 }
 close LOGFILE;
 close INFILE;
-unlink($filename . ".copy"); # delete the copy 
+unlink($infilename . ".copy"); # delete the copy
--- www/submit.php
+++ www/submit.php
@@ -1,28 +1,55 @@
 <?php
 
-
-// write the username and password to the file
-if (empty($_POST["name"])) {
-	echo "no name";
-	return;
+function isStringAllowed($input) {
+  $allowed = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+  $chars = str_split($input);
+  for($i=0;$i<count($chars);$i++){
+    if(strstr($allowed, $chars[$i]) == false ) {
+      echo $chars[$i] . "<br/>";
+      return false;
+    }
+  }
+  return true;
 }
 
-if (empty($_POST["password"])) {
-	echo "no name";
-	return;
+function getArg($argname) {
+  if (empty($_POST[$argname])) {
+    echo "no $argname";
+    return "";
+  } else {
+    $arg = $_POST[$argname];
+    if (!(isStringAllowed($arg))) {
+      echo "illegal character in $argname";
+      return "";
+    }
+  }
+  return $_POST[$argname];
 }
+
+$name = getArg("name");
+if (empty($name)) {return;}
+
+$password = getArg("password");
+if (empty($password)) {return;}
+
+$passwordverify = getArg("passwordverify");
+if (empty($passwordverify)) {return;}
+
+if (strcmp($password, $passwordverify) != 0) {echo "password and passwordverify do not match, please try again."; return; }
 
 if (empty($_POST["email"])) {
-	echo "no email";
-	return;
+  echo "no email";
+  return;
+} else {
+  $email = $_POST["email"];
 }
 
 $myFile = "/usr/local/etc/tarbackup/userstocreate.txt";
 $fh = fopen($myFile, 'a') or die("can't open file");
-$stringData = $_POST["name"] . ":" . $_POST["password"] . ":" . $_POST["email"] . "\n";
+$stringData = $name . ":" . $password . ":" . $email . "\n";
 fwrite($fh, $stringData);
 fclose($fh);
 
-echo "user created";
+echo "user '$name' created.";
 
 ?>
